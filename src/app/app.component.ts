8f9d7d9646914214ab0e43259331221b7191259d
Adrian Mohnacs <amohnacs@gmail.com>
2023-07-10 11:42:00 +0700

    Firebase hosting for EZ Bild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAdRlsZX4AekDNM50um5wuIY-fJ_AjGwz4",
+  authDomain: "startupdashboard.firebaseapp.com",
+  projectId: "startupdashboard",
+  storageBucket: "startupdashboard.appspot.com",
+  messagingSenderId: "1003287716784",
+  appId: "1:1003287716784:web:c94d3319ca482d51956c75",
+  measurementId: "G-SQ5DD9JWHR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 @Component({
     selector: 'app-root',
